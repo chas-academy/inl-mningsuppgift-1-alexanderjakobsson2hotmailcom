cb70e253768358e5a89353613db5f139b19ce3f5
alexander_jakobsson2@hotmail.com <alexander_jakobsson2@hotmail.com>
2025-06-23 16:14:31 +0200

    First try

--- main.c
+++ main.c
@@ -8,6 +8,47 @@ int main()
 
    
     // Your code goes here
+    int dice_throws[100];
+    int sum = 0;
+    for(int i = 0; i < 100; i++){
+        dice_throws[i] = (rand() %6) + 1;
+        printf("roll %d\n", dice_throws[i]);
+        sum += dice_throws[i];
+    }
 
+    int sum1 = 0;
+    int sum2 = 0;
+    int sum3 = 0;
+    int sum4 = 0;
+    int sum5 = 0;
+    int sum6 = 0;
+    for(int i = 0; i < 100; i++){
+        if(dice_throws[i] == 1){
+        sum1++;
+        }
+        if(dice_throws[i] == 2){
+        sum2++;
+        }
+        if(dice_throws[i] == 3){
+        sum3++;
+        }
+        if(dice_throws[i] == 4){
+        sum4++;
+        }
+        if(dice_throws[i] == 5){
+        sum5++;
+        }
+        if(dice_throws[i] == 6){
+        sum6++;
+        }
+    }
+    printf("%d\n", sum1);
+    printf("%d\n", sum2);
+    printf("%d\n", sum3);
+    printf("%d\n", sum4);
+    printf("%d\n", sum5);
+    printf("%d\n", sum6);
+    printf("%d\n", sum);
+    printf("%1f\n", sum/100.0);
     return 0;
 }
